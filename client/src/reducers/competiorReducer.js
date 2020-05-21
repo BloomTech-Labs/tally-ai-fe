25f6a07304dfe45398553a460d73aaecfdd429ab
Benjamin Koehler <bkoehler751@gmail.com>
2020-05-20 19:22:37 -0700

    moved competitor reducer

---
+++ client/src/reducers/competiorReducer.js
@@ -0,0 +1,14 @@
+ import{
+ //adding competitors to user's competitor list
+ ADD_COMPETITOR_START,
+ ADD_COMPETITOR_SUCCESS,
+ ADD_COMPETITOR_FAILURE,
+
+ //removing competitors from user's competitor list
+ REMOVE_COMPETITOR_START,
+ REMOVE_COMPETITOR_SUCCESS,
+ REMOVE_COMPETITOR_FAILURE,
+ } from '../actions/index'
+import competitorReducer from '.';
+
+export default competitorReducer
