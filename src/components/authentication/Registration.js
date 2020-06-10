4b55c2bbc19f2c56d887fa6a1445cb08630dd74d
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-06-09 22:22:04 -0400

    Finished state/actions imports on components

--- src/components/authentication/Registration.js
+++ src/components/authentication/Registration.js
@@ -1,212 +1,244 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { shouldUpdateLoggedInUser } from "../../actions/index";
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { shouldUpdateLoggedInUser } from '../../actions/settingsActions'
 
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import {Link} from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
-import axios from "axios"
+import axios from 'axios'
 
 const useStyles = makeStyles(theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexDirection: 'column',
-        alignItems: 'center',
-        paddingTop: '7%'
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: '40%'
-    },
-    dense: {
-        marginTop: theme.spacing(2),
-    },
-    button: {
-        marginTop: "30px", 
-        backgroundColor: "#1E4DC7", 
-        color: "white",
-        margin: theme.spacing(1),
-        marginBottom: '30px',
-        width: '15%'
-    },
-    input: {
-        display: 'none',
-    },
-}));
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		flexDirection: 'column',
+		alignItems: 'center',
+		paddingTop: '7%'
+	},
+	textField: {
+		marginLeft: theme.spacing(1),
+		marginRight: theme.spacing(1),
+		width: '40%'
+	},
+	dense: {
+		marginTop: theme.spacing(2)
+	},
+	button: {
+		marginTop: '30px',
+		backgroundColor: '#1E4DC7',
+		color: 'white',
+		margin: theme.spacing(1),
+		marginBottom: '30px',
+		width: '15%'
+	},
+	input: {
+		display: 'none'
+	}
+}))
 
 function Registration(props) {
-    const classes = useStyles();
-    const [userCredentials, setCredentials] = useState({
-        first_name: "",
-        last_name: "",
-        email: "",
-        confirmedEmail: "",
-        password: "",
-        confirmedPassword: ""
-    });
+	const classes = useStyles()
+	const [userCredentials, setCredentials] = useState({
+		first_name: '',
+		last_name: '',
+		email: '',
+		confirmedEmail: '',
+		password: '',
+		confirmedPassword: ''
+	})
 
-    // useEffect(() => {
-    //     if (props.loggedInUser) {//we're logged in, lets redirect to /home
-    //         props.history.push('/')
-    //     }
-    // }, [props.loggedInUser]);
+	// useEffect(() => {
+	//     if (props.loggedInUser) {//we're logged in, lets redirect to /home
+	//         props.history.push('/')
+	//     }
+	// }, [props.loggedInUser]);
 
-    const submitHandler = event => {
-        console.log("onSubmit working", props.isFetching);
-        event.preventDefault();
+	const submitHandler = event => {
+		console.log('onSubmit working', props.isFetching)
+		event.preventDefault()
 
-        if(!(userCredentials.confirmedEmail === userCredentials.email)){
-            alert("Your confirmed email does not match.");
-            return;
-        }
+		if (!(userCredentials.confirmedEmail === userCredentials.email)) {
+			alert('Your confirmed email does not match.')
+			return
+		}
 
-        if(!(userCredentials.password === userCredentials.confirmedPassword)){
-            alert("Your confirmed password does not match.");
-            return;
-        }
+		if (!(userCredentials.password === userCredentials.confirmedPassword)) {
+			alert('Your confirmed password does not match.')
+			return
+		}
 
-        if (!props.isFetching) {//don't let them submit again if the backend is already processing their registration request
-            console.log(userCredentials);
+		if (!props.isFetching) {
+			//don't let them submit again if the backend is already processing their registration request
+			console.log(userCredentials)
 
-            let formattedUserCredentials = userCredentials;
-            delete formattedUserCredentials.confirmedEmail;
-            delete formattedUserCredentials.confirmedPassword;
+			let formattedUserCredentials = userCredentials
+			delete formattedUserCredentials.confirmedEmail
+			delete formattedUserCredentials.confirmedPassword
 
-            axios
-                .post(`https://cors-anywhere.herokuapp.com/http://tallyai.us-east-1.elasticbeanstalk.com/api/auth/register`, formattedUserCredentials)
-                .then(
-                    res => {
-                        console.log("Registered successfully", res);
-                        localStorage.setItem("token", res.data.token);
-                        localStorage.setItem("userID", res.data.userN.id);
-                        props.shouldUpdateLoggedInUser(true);
-                        props.history.push('/search/business');
-                    }
-                )
-                .catch(err => {
-                    console.log("Error registering", err);
-                }
-                );
-        }
-    }
+			axios
+				.post(
+					`https://cors-anywhere.herokuapp.com/http://tallyai.us-east-1.elasticbeanstalk.com/api/auth/register`,
+					formattedUserCredentials
+				)
+				.then(res => {
+					console.log('Registered successfully', res)
+					localStorage.setItem('token', res.data.token)
+					localStorage.setItem('userID', res.data.userN.id)
+					props.shouldUpdateLoggedInUser(true)
+					props.history.push('/search/business')
+				})
+				.catch(err => {
+					console.log('Error registering', err)
+				})
+		}
+	}
 
-    const changeHandler = event => {
-        setCredentials({ ...userCredentials, [event.target.name]: event.target.value })
-    }
+	const changeHandler = event => {
+		setCredentials({
+			...userCredentials,
+			[event.target.name]: event.target.value
+		})
+	}
 
-    return (
-        <div style={{background: "linear-gradient(341.24deg, #B5E4FE 11.16%, #BDF5FF 82.03%)", height:"100vh" }}>
-            <form className={classes.container} onSubmit={(e) => submitHandler(e)}>
-                <div style={{ width: "80%", marginLeft: "10%", marginRight: "10%", borderRadius: "47px", marginTop: "50px", marginBottom: "50px", height: "70vh", background: "white" }}>
-                    <div style={{ width: "91%", marginLeft: "9%", textAlign: "left", fontSize: "24px", paddingTop: "25px" }}>
-                        <h1>Sign up for an account</h1>
-                    </div>
-                    <div style={{ width: "100%" }}>
-                        <TextField
-                            label="First Name"
-                            type="text"
-                            name="first_name"
-                            className={classes.textField}
-                            value={userCredentials.first_name}
-                            onChange={changeHandler}
-                            placeholder="First Name"
-                            required
-                            variant="outlined"
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Last Name"
-                            type="text"
-                            name="last_name"
-                            className={classes.textField}
-                            value={userCredentials.last_name}
-                            onChange={changeHandler}
-                            placeholder="Last Name"
-                            required
-                            variant="outlined"
-                            margin="normal"
-                        />
-                    </div>
-                    <div style={{ width: "100%", paddingTop: "25px", paddingBottom: "25px" }}>
-                        <TextField
-                            label="Email"
-                            type="email"
-                            name="email"
-                            className={classes.textField}
-                            value={userCredentials.email}
-                            onChange={changeHandler}
-                            placeholder="Email"
-                            required
-                            variant="outlined"
-                            margin="normal"
-                        />
-                        <TextField
-                            label="Confirm Email"
-                            type="email"
-                            name="confirmedEmail"
-                            className={classes.textField}
-                            value={userCredentials.confirmedEmail}
-                            onChange={changeHandler}
-                            placeholder="Email"
-                            required
-                            variant="outlined"
-                            margin="normal"
-                        />
-                    </div>
-                    <div style={{ width: "100%", paddingBottom: "25px" }}>
-                        <TextField
-                            type="password"
-                            name="password"
-                            className={classes.textField}
-                            value={userCredentials.password}
-                            onChange={changeHandler}
-                            placeholder="Password"
-                            required
-                            variant="outlined"
-                            margin="normal"
-
-                        />
-                        <TextField
-                            type="password"
-                            name="confirmedPassword"
-                            className={classes.textField}
-                            value={userCredentials.confirmedPassword}
-                            onChange={changeHandler}
-                            placeholder="Confirm Password"
-                            required
-                            variant="outlined"
-                            margin="normal"
-
-                        />
-                    </div>
-                    <div style={{ width: "100%" }}>
-                        <Button className={classes.button} variant="outlined" type="submit">Register</Button>
-                    </div>
-                    <div>
-                        <p>
-                            Already have an account? <Link style={{padding: "0", color: "black"}} to='/Login/'>Sign in here</Link>
-                        </p>
-                    </div>
-                </div>
-            </form>
-        </div>
-    )
+	return (
+		<div
+			style={{
+				background:
+					'linear-gradient(341.24deg, #B5E4FE 11.16%, #BDF5FF 82.03%)',
+				height: '100vh'
+			}}
+		>
+			<form className={classes.container} onSubmit={e => submitHandler(e)}>
+				<div
+					style={{
+						width: '80%',
+						marginLeft: '10%',
+						marginRight: '10%',
+						borderRadius: '47px',
+						marginTop: '50px',
+						marginBottom: '50px',
+						height: '70vh',
+						background: 'white'
+					}}
+				>
+					<div
+						style={{
+							width: '91%',
+							marginLeft: '9%',
+							textAlign: 'left',
+							fontSize: '24px',
+							paddingTop: '25px'
+						}}
+					>
+						<h1>Sign up for an account</h1>
+					</div>
+					<div style={{ width: '100%' }}>
+						<TextField
+							label='First Name'
+							type='text'
+							name='first_name'
+							className={classes.textField}
+							value={userCredentials.first_name}
+							onChange={changeHandler}
+							placeholder='First Name'
+							required
+							variant='outlined'
+							margin='normal'
+						/>
+						<TextField
+							label='Last Name'
+							type='text'
+							name='last_name'
+							className={classes.textField}
+							value={userCredentials.last_name}
+							onChange={changeHandler}
+							placeholder='Last Name'
+							required
+							variant='outlined'
+							margin='normal'
+						/>
+					</div>
+					<div
+						style={{ width: '100%', paddingTop: '25px', paddingBottom: '25px' }}
+					>
+						<TextField
+							label='Email'
+							type='email'
+							name='email'
+							className={classes.textField}
+							value={userCredentials.email}
+							onChange={changeHandler}
+							placeholder='Email'
+							required
+							variant='outlined'
+							margin='normal'
+						/>
+						<TextField
+							label='Confirm Email'
+							type='email'
+							name='confirmedEmail'
+							className={classes.textField}
+							value={userCredentials.confirmedEmail}
+							onChange={changeHandler}
+							placeholder='Email'
+							required
+							variant='outlined'
+							margin='normal'
+						/>
+					</div>
+					<div style={{ width: '100%', paddingBottom: '25px' }}>
+						<TextField
+							type='password'
+							name='password'
+							className={classes.textField}
+							value={userCredentials.password}
+							onChange={changeHandler}
+							placeholder='Password'
+							required
+							variant='outlined'
+							margin='normal'
+						/>
+						<TextField
+							type='password'
+							name='confirmedPassword'
+							className={classes.textField}
+							value={userCredentials.confirmedPassword}
+							onChange={changeHandler}
+							placeholder='Confirm Password'
+							required
+							variant='outlined'
+							margin='normal'
+						/>
+					</div>
+					<div style={{ width: '100%' }}>
+						<Button className={classes.button} variant='outlined' type='submit'>
+							Register
+						</Button>
+					</div>
+					<div>
+						<p>
+							Already have an account?{' '}
+							<Link style={{ padding: '0', color: 'black' }} to='/Login/'>
+								Sign in here
+							</Link>
+						</p>
+					</div>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 const mapStateToProps = state => {
-    return {
-        isFetching: state.loggedInUser.isFetching,
-        error: state.loggedInUser.error,
-        loggedInUser: state.loggedInUser
-    };
-};
+	return {
+		isFetching: state.settings.loggedInUser.isFetching,
+		error: state.settings.loggedInUser.error,
+		loggedInUser: state.settings.loggedInUser
+	}
+}
 
-export default connect(
-    mapStateToProps,
-    { shouldUpdateLoggedInUser }
-)(Registration)
-
+export default connect(mapStateToProps, { shouldUpdateLoggedInUser })(
+	Registration
+)
--- src/reducers/businessReducer.js
+++ src/reducers/businessReducer.js
@@ -1,166 +1,181 @@
 import {
-  // Yelp Business Search
-  FETCH_BUSINESS_START,
-  FETCH_BUSINESS_SUCCESS,
-  FETCH_BUSINESS_FAILURE,
-  // Select business from results
-  SELECT_BUSINESS_START,
-  SELECT_BUSINESS_SUCCESS,
-  SELECT_BUSINESS_FAILURE,
+	// Yelp Business Search
+	FETCH_BUSINESS_START,
+	FETCH_BUSINESS_SUCCESS,
+	FETCH_BUSINESS_FAILURE,
+	// Select business from results
+	SELECT_BUSINESS_START,
+	SELECT_BUSINESS_SUCCESS,
+	SELECT_BUSINESS_FAILURE,
 
-  //adding businesses to user's owned businesses list
-  ADD_BUSINESS_START,
-  ADD_BUSINESS_SUCCESS,
-  ADD_BUSINESS_FAILURE,
+	//adding businesses to user's owned businesses list
+	ADD_BUSINESS_START,
+	ADD_BUSINESS_SUCCESS,
+	ADD_BUSINESS_FAILURE,
 
-  //removing businesses from user's owned businesses list
-  REMOVE_BUSINESS_START,
-  REMOVE_BUSINESS_SUCCESS,
-  REMOVE_BUSINESS_FAILURE} from '../actions/businessActions'
+	//removing businesses from user's owned businesses list
+	REMOVE_BUSINESS_START,
+	REMOVE_BUSINESS_SUCCESS,
+	REMOVE_BUSINESS_FAILURE
+} from '../actions/businessActions'
 
-  const initialState = {   
-    userBusinesses: {
-      isSetting: false,
-      error: null,
-      businesses: []
-    },
+const initialState = {
+	userBusinesses: {
+		isSetting: false,
+		error: null,
+		businesses: []
+	},
 
-    currentlySelectedBusiness: {
-      businessId: null, //default tab selected by default
-      // for side bar
-      businessName: null,
-      businessImg: null,
-      // for top-of-page info cards
-      reviewCount: 0,
-      averageRating: 0,
-      changeInRating: "",
-      address: ""
-    },
+	currentlySelectedBusiness: {
+		businessId: null, //default tab selected by default
+		// for side bar
+		businessName: null,
+		businessImg: null,
+		// for top-of-page info cards
+		reviewCount: 0,
+		averageRating: 0,
+		changeInRating: '',
+		address: ''
+	},
 
-    searchResults: {
-      isFetching: false,
-      error: null,
-      data: null
-    },
+	searchResults: {
+		isFetching: false,
+		error: null,
+		data: null
+	}
+}
 
-  };
+function businessReducer(state = initialState, action) {
+	switch (action.type) {
+		case FETCH_BUSINESS_START:
+			return {
+				...state,
+				searchResults: {
+					...state.searchResults,
+					isFetching: true,
+					error: null
+				}
+			}
+		case FETCH_BUSINESS_SUCCESS:
+			return {
+				...state,
+				searchResults: {
+					...state.searchResults,
+					isFetching: false,
+					data: action.payload,
+					error: null
+				}
+			}
+		case FETCH_BUSINESS_FAILURE:
+			return {
+				...state,
+				searchResults: {
+					...state.searchResults,
+					isFetching: false,
+					error: action.payload
+				}
+			}
 
-  function businessReducer(state = initialState, action) {
-    switch (action.type) {
-    case FETCH_BUSINESS_START:
-      return {
-        ...state,
-        searchResults: {
-          ...state.searchResults,
-          isFetching: true,
-          error: null
-        }
-      };
-    case FETCH_BUSINESS_SUCCESS:
-      return {
-        ...state,
-        searchResults: {
-          ...state.searchResults,
-          isFetching: false,
-          data: action.payload,
-          error: null
-        }
-      };
-    case FETCH_BUSINESS_FAILURE:
-      return {
-        ...state,
-        searchResults: {
-          ...state.searchResults,
-          isFetching: false,
-          error: action.payload
-        }
-      };
+		case SELECT_BUSINESS_START:
+			return {
+				...state,
+				currentlySelectedBusiness: { ...action.payload } //set data immediatly (missing data that we're waiting on Yelp for)
+			}
+		case SELECT_BUSINESS_SUCCESS:
+			return {
+				...state,
+				currentlySelectedBusiness: { ...action.payload } //fill in the new address, rating and review count info
+			}
+		case SELECT_BUSINESS_FAILURE:
+			return {
+				...state,
+				currentlySelectedBusiness: { ...action.payload } //revert back to old data
+			}
 
-      case ADD_BUSINESS_START:
-      console.log("ADDDDDDD BUSINESSS STARTTT");
-      return {
-        ...state,
-        userBusinesses: {
-          ...state.userBusinesses,
-          isSetting: true,
-          error: null
-        }
-      };
-    case ADD_BUSINESS_SUCCESS:
-      return {
-        ...state,
-        userBusinesses: {
-          businesses: action.payload.businesses.map(business => {
-            return {
-              id: business.id,
-              businessId: business.yelp_id, //default tab selected by default
-              // for side bar
-              businessName: business.name,
-              businessImg: business.image_url,
-              // for top-of-page info cards
-              reviewCount: 0,
-              averageRating: 0,
-              changeInRating: ""
-            };
-          }),
-          isSetting: false,
-          error: null
-        }
-      };
-    case ADD_BUSINESS_FAILURE:
-      return {
-        ...state,
-        userBusinesses: {
-          ...state.userBusinesses,
-          isSetting: false,
-          error: action.payload
-        }
-      };
+		case ADD_BUSINESS_START:
+			console.log('ADDDDDDD BUSINESSS STARTTT')
+			return {
+				...state,
+				userBusinesses: {
+					...state.userBusinesses,
+					isSetting: true,
+					error: null
+				}
+			}
+		case ADD_BUSINESS_SUCCESS:
+			return {
+				...state,
+				userBusinesses: {
+					businesses: action.payload.businesses.map(business => {
+						return {
+							id: business.id,
+							businessId: business.yelp_id, //default tab selected by default
+							// for side bar
+							businessName: business.name,
+							businessImg: business.image_url,
+							// for top-of-page info cards
+							reviewCount: 0,
+							averageRating: 0,
+							changeInRating: ''
+						}
+					}),
+					isSetting: false,
+					error: null
+				}
+			}
+		case ADD_BUSINESS_FAILURE:
+			return {
+				...state,
+				userBusinesses: {
+					...state.userBusinesses,
+					isSetting: false,
+					error: action.payload
+				}
+			}
 
+		case REMOVE_BUSINESS_START:
+			return {
+				...state,
+				userBusinesses: {
+					...state.userBusinesses,
+					isSetting: true,
+					error: null
+				}
+			}
 
-      case REMOVE_BUSINESS_START:
-      return {
-        ...state,
-        userBusinesses: {
-          ...state.userBusinesses,
-          isSetting: true,
-          error: null
-        }
-      };
-
-    case REMOVE_BUSINESS_SUCCESS:
-      return {
-        ...state,
-        userBusinesses: {
-          businesses: action.payload.businesses.map(business => {
-            return {
-              id: business.id,
-              businessId: business.yelp_id, //default tab selected by default
-              // for side bar
-              businessName: business.name,
-              businessImg: business.image_url,
-              // for top-of-page info cards
-              reviewCount: 0,
-              averageRating: 0,
-              changeInRating: ""
-            };
-          }),
-          isSetting: false,
-          error: null
-        }
-      };
-    case REMOVE_BUSINESS_FAILURE:
-      return {
-        ...state,
-        userBusinesses: {
-          ...state.userBusinesses,
-          isSetting: false,
-          error: action.payload
-        }
-      };
-      default:     
-      return state
-  }
+		case REMOVE_BUSINESS_SUCCESS:
+			return {
+				...state,
+				userBusinesses: {
+					businesses: action.payload.businesses.map(business => {
+						return {
+							id: business.id,
+							businessId: business.yelp_id, //default tab selected by default
+							// for side bar
+							businessName: business.name,
+							businessImg: business.image_url,
+							// for top-of-page info cards
+							reviewCount: 0,
+							averageRating: 0,
+							changeInRating: ''
+						}
+					}),
+					isSetting: false,
+					error: null
+				}
+			}
+		case REMOVE_BUSINESS_FAILURE:
+			return {
+				...state,
+				userBusinesses: {
+					...state.userBusinesses,
+					isSetting: false,
+					error: action.payload
+				}
+			}
+		default:
+			return state
+	}
 }
 
 export default businessReducer
