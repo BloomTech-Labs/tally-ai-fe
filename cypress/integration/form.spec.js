6623c4c1729a638594e2a498f596375829d0ded9
Benjamin Koehler <bkoehler751@gmail.com>
2020-06-17 19:43:19 -0700

    Added test for input for registration

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -6,4 +6,22 @@ describe('Form', () => {
     it('it focuses the input', () => {
       cy.focused().should('have.id', 'firstName')
     })
+    it('accepts input', () => {
+        const input = "First Name"
+        cy.get('#firstName')
+          .type(input)
+          .should('have.value', input)
+      })
+      it('accepts another input', () => {
+          const input2 = "Last Name"
+          cy.get('#lastName')
+          .type(input2)
+          .should('have.value', input2)
+      })
+      it('accepts email', () => {
+        const email = "test123@test.com"
+        cy.get('#email')
+        .type(email)
+        .should('have.value', email)
+    })
   })
