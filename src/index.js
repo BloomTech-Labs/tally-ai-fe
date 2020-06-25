80526dd21340b049ee005433a45af14a7a250b2f
adrianbparra <adrianplane12@gmail.com>
2020-06-24 18:38:00 -0700

    fix: added store for cypress testing

--- src/index.js
+++ src/index.js
@@ -34,3 +34,8 @@ document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// expose store when run in Cypress
+if (window.Cypress) {
+    window.store = store
+}
