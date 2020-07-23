1d14bc83caabd583f1d5ea8ce4ed90ee653c42a5
Raudel Flores <55217618+raudelf@users.noreply.github.com>
2020-07-22 21:30:32 -0500

    Refactor/Cleanup Components with Inline Styles (#117)
    * created the aboutus sass file
    
    * cleaned up indentations
    
    * cleaned up indentations
    
    * removed commented out code
    
    * imported sass styling and removed inline styles
    
    * refactored to use scss instead of inline styles
    
    * :recycle: Inline Styles to SCSS
    
    Refactored code to use scss instead of inline styles and removed the inline styles from the component
    
    * :recycle: Inline-Styles to SASS
    
    Removed all of the inline styles from the component along with commented out styles. Moved everything over to the navbar sass file
    
    * :pencil2: Missed Some
    
    Imported sass file to apply styles and removed inline-styles I missed
    
    * :fire: Removed Code
    
    Removed code unnecessary code
    
    * :recycle: Inline Styles to SASS
    
    Removed all inlined styles and moved it over to the search sass file. I also imported the styles to be applied
    
    * :recycle: Inline Styles to SASS
    
    Removed all of the unecessary inline styles and moved them over to the searchPage scss file. I also removed commented out code and removed some objects from the useStyles function that were not being used. Finally imported the sass file to apply the styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { setUserInfo, getUserInfo, shouldUpdateLoggedInUser } from "./actions/settingsActions";
+import {
+  setUserInfo,
+  getUserInfo,
+  shouldUpdateLoggedInUser,
+} from "./actions/settingsActions";
 import PrivateRoute from "./auth/PrivateRoute";
 import PublicRoute from "./auth/PublicRoute";
-import SearchPage from "./components/SearchPage"
+import SearchPage from "./components/SearchPage";
 // Components
-import RestrictMobile from "./components/RestrictMobile"
+import RestrictMobile from "./components/RestrictMobile";
 import NavBar from "./components/navbar";
 import Footer from "./components/footer";
 import Search from "./components/search.js";
@@ -21,103 +25,66 @@ import AboutUs from "./components/aboutus";
 import DashboardPlus from "./components/dashboard/dashboardPlus";
 import Policy from "./components/TOS/legal";
 
-import { widgets } from "./components/WidgetSystem/WidgetRegistry"
+import { widgets } from "./components/WidgetSystem/WidgetRegistry";
 
 function App(props) {
-	useEffect(() => {
-		console.log('getting user data');
-		if (localStorage.getItem('token') && localStorage.getItem('userID')) {
-			//we're logged in but there's no user info in the store, lets fix that
+  useEffect(() => {
+    console.log("getting user data");
+    if (localStorage.getItem("token") && localStorage.getItem("userID")) {
+      //we're logged in but there's no user info in the store, lets fix that
 
-			props.getUserInfo(localStorage.getItem('userID'));
-		} else {
-			//do we need to delete anything from state when they log out?
-			let userInfo = {
-				competitors: [],
-				loggedInUser: { firstName: null, lastName: null },
-				businesses: [],
-				activeWidgets: [widgets[0].name, widgets[1].name],
-				activeTabs: ['defaultTab'],
-				currentlySelectedBusiness: {
-					businessId: null,
-					businessName: null,
-					businessImg: null,
-					reviewCount: 0,
-					averageRating: 0,
-					changeInRating: ''
-				}
-			};
+      props.getUserInfo(localStorage.getItem("userID"));
+    } else {
+      //do we need to delete anything from state when they log out?
+      let userInfo = {
+        competitors: [],
+        loggedInUser: { firstName: null, lastName: null },
+        businesses: [],
+        activeWidgets: [widgets[0].name, widgets[1].name],
+        activeTabs: ["defaultTab"],
+        currentlySelectedBusiness: {
+          businessId: null,
+          businessName: null,
+          businessImg: null,
+          reviewCount: 0,
+          averageRating: 0,
+          changeInRating: "",
+        },
+      };
 
-			props.setUserInfo(userInfo);
-		}
-		props.shouldUpdateLoggedInUser(false);
-	}, [props.loggedInUser.shouldUpdate]);
+      props.setUserInfo(userInfo);
+    }
+    props.shouldUpdateLoggedInUser(false);
+  }, [props.loggedInUser.shouldUpdate]);
 
-	return (
-		<div className='App'>
-			<RestrictMobile />
-			<NavBar />
-			<PublicRoute exact path='/' component={Search} />
-			<Route path='/Dashboard/' component={DashboardGrid} />
-			<Route path='/Register/' component={Registration} />
-			<Route path='/Login/' component={Login} />
-			<Route path='/Compset' component={CompSet} />
-			<Route path='/About' component={AboutUs} />
-			<Route path='/Legal/:doc' component={Policy} />
-			<Route path='/DashboardPlus/' component={DashboardPlus} />
-			<PrivateRoute path='/Settings/' component={Settings} />
-			<PrivateRoute path='/Search/:searchMode' exact component={SearchPage} />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <RestrictMobile />
+      <NavBar />
+      <PublicRoute exact path="/" component={Search} />
+      <Route path="/Dashboard/" component={DashboardGrid} />
+      <Route path="/Register/" component={Registration} />
+      <Route path="/Login/" component={Login} />
+      <Route path="/Compset" component={CompSet} />
+      <Route path="/About" component={AboutUs} />
+      <Route path="/Legal/:doc" component={Policy} />
+      <Route path="/DashboardPlus/" component={DashboardPlus} />
+      <PrivateRoute path="/Settings/" component={Settings} />
+      <PrivateRoute path="/Search/:searchMode" exact component={SearchPage} />
+      <Footer />
+    </div>
+  );
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   loggedInUser: state.settings.loggedInUser,
-  activeTabs: state.settings.activeTabs
+  activeTabs: state.settings.activeTabs,
 });
 
 export default withRouter(
-	connect(mapStateToProps, {
-		setUserInfo,
-		getUserInfo,
-		shouldUpdateLoggedInUser
-	})(App)
+  connect(mapStateToProps, {
+    setUserInfo,
+    getUserInfo,
+    shouldUpdateLoggedInUser,
+  })(App)
 );
-
-// {
-//   "first_name": string,
-//   "last_name": string,
-//   "businesses": [
-//       {
-//           "id": integer,
-//           "name": string,
-//           "location": {
-//               "city": string,
-//               "state": string
-//           }
-//           "yelp": {
-//               "id": string,
-//               "yelp_id": string,
-//               "url": string,
-//               "image_url": string
-//           }
-//       },
-//   ],
-//   "favorites": [
-//     {
-//         "id": integer,
-//         "name": string,
-//         "location": {
-//             "city": string,
-//             "state": string
-//         }
-//         "yelp": {
-//             "id": string,
-//             "yelp_id": string,
-//             "url": string,
-//             "image_url": string
-//         }
-//     },
-// ]
-// }
--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,82 +1,79 @@
-import React, { useEffect, useState } from "react";
-import Daniel from "./images/Daniel.png"
-import David from "./images/David.png"
-import Patrick from "./images/Patrick.png"
-import Steve from "./images/Steve.png"
-import Wenjing from "./images/Wenjing.png"
-import Rohan from "./images/Rohan.png"
-import Lily from "./images/Lily.png"
-import Colton from "./images/Colton.png"
-import Liz from "./images/Liz.png"
+import React from "react";
+import Daniel from "./images/Daniel.png";
+import David from "./images/David.png";
+import Patrick from "./images/Patrick.png";
+import Steve from "./images/Steve.png";
+import Wenjing from "./images/Wenjing.png";
+import Rohan from "./images/Rohan.png";
+import Lily from "./images/Lily.png";
+import Colton from "./images/Colton.png";
+import Liz from "./images/Liz.png";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileExcel } from "@fortawesome/free-solid-svg-icons";
-// import { faTwitter } from "@fortawesome/free-solid-svg-icons"; 
+import "./aboutus.scss";
 
 const AboutUs = () => {
-    return(
-        <div>
-            <div className="MeetTheTeam" style={{paddingTop: "10vh"}}>
-                <h1 style={{fontWeight: "800", fontSize: "80px", lineHeight: "109px", letterSpacing: "0.25px"}}>Meet The Team</h1>
-            </div>
+  return (
+    <div>
+      <div className="MeetTheTeam">
+        <h1>Meet The Team</h1>
+      </div>
 
-            <div className="WebDev" style={{display: "flex", flexDirection: "row", marginLeft:"10%", marginRight:"10%", marginTop:"5%", justifyContent: "space-between" }}>
-                <div className="WebTeam" style={{width: "22%"}}>
-                    <img src={Daniel} alt="Photo of developer" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Daniel Firpo</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Web Developer</h3>
-                    {/* <FontAwesomeIcon icon={faTwitter} size="6x" style={{color: '#15aabf'}} /> */}
-                </div>
-                <div className="WebTeam" style={{width: "22%"}}>
-                    <img src={David} alt="Photo of developer" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>David Downes</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Web Developer</h3>
-                </div>
-                <div className="WebTeam" style={{width: "22%"}}>
-                    <img src={Patrick} alt="Photo of developer" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Patrick Stevenson</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Web Developer</h3>
-                </div>
-                <div className="WebTeam" style={{width: "22%"}}>
-                    <img src={Steve} alt="Photo of developer" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Steve Renner</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Web Developer</h3>
-                </div>
-            </div>
-
-            <div className="DataScientist" style={{display: "flex", flexDirection: "row", marginLeft:"10%", marginRight:"10%", justifyContent: "space-evenly", marginTop:"100px" }}>
-                <div className="DSTeam" style={{width: "20%"}}>
-                    <img src={Lily} alt="Photo of Data Scientist" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Lily Su</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Data Scientist</h3>
-                </div>
-                <div className="DSTeam" style={{width: "20%"}}>
-                    <img src={Rohan} alt="Photo of Data Scientist" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Rohan Kulkarni</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Data Scientist</h3>
-                </div>
-                <div className="DSTeam" style={{width: "20%"}}>
-                    <img src={Wenjing} alt="Photo of Data Scientist" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Wenjing Liu</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Data Scientist</h3>
-                </div>
-            </div>
-
-            <div className="Leads" style={{display: "flex", flexDirection: "row", marginLeft:"10%", marginRight:"10%", justifyContent: "center", marginTop:"100px", paddingBottom: "80px" }}>
-                <div className="LeadUX" style={{width: "20%"}}>
-                    <img src={Colton} alt="Photo of UX Designer" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Colton Mortenson</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>UI/UX Designer</h3>
-                </div>
-                <div className="LeadUX" style={{width: "20%", marginLeft: "150px"}}>
-                    <img src={Liz} alt="Photo of Data Scientist" style={{borderRadius:"100%", height: '18vh'}} />
-                    <h1 style={{width: "120%", marginLeft: "-20px", fontStyle: 'normal', fontWeight: '500', fontSize: '22px', lineHeight: '25px'}}>Elizabeth Ter Sahakyan</h1>
-                    <h3 style={{fontStyle: 'normal', fontWeight: '500', fontSize: '15px', lineHeight: '15px'}}>Team Lead</h3>
-                </div>
-            </div>
-
+      <div className="WebDev">
+        <div className="WebTeam">
+          <img src={Daniel} alt="Photo of developer" />
+          <h1>Daniel Firpo</h1>
+          <h3>Web Developer</h3>
+          {/* <FontAwesomeIcon icon={faTwitter} size="6x" style={{color: '#15aabf'}} /> */}
         </div>
-    );
-}
+        <div className="WebTeam">
+          <img src={David} alt="Photo of developer" />
+          <h1>David Downes</h1>
+          <h3>Web Developer</h3>
+        </div>
+        <div className="WebTeam">
+          <img src={Patrick} alt="Photo of developer" />
+          <h1>Patrick Stevenson</h1>
+          <h3>Web Developer</h3>
+        </div>
+        <div className="WebTeam">
+          <img src={Steve} alt="Photo of developer" />
+          <h1>Steve Renner</h1>
+          <h3>Web Developer</h3>
+        </div>
+      </div>
+
+      <div className="DataScientist">
+        <div className="DSTeam">
+          <img src={Lily} alt="Photo of Data Scientist" />
+          <h1>Lily Su</h1>
+          <h3>Data Scientist</h3>
+        </div>
+        <div className="DSTeam">
+          <img src={Rohan} alt="Photo of Data Scientist" />
+          <h1>Rohan Kulkarni</h1>
+          <h3>Data Scientist</h3>
+        </div>
+        <div className="DSTeam">
+          <img src={Wenjing} alt="Photo of Data Scientist" />
+          <h1>Wenjing Liu</h1>
+          <h3>Data Scientist</h3>
+        </div>
+      </div>
+
+      <div className="Leads">
+        <div className="LeadUX">
+          <img src={Colton} alt="Photo of UX Designer" />
+          <h1>Colton Mortenson</h1>
+          <h3>UI/UX Designer</h3>
+        </div>
+        <div className="LeadUX" id="leadUX">
+          <img src={Liz} alt="Photo of Data Scientist" />
+          <h1>Elizabeth Ter Sahakyan</h1>
+          <h3>Team Lead</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
