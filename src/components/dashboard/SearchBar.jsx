a7798e156b08f4c2ae3f387c0d49f662aafd3ad7
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-07-28 22:44:58 -0400

    :lipstick: Made a few changes to the UI to match the Figma design

--- src/components/dashboard/SearchBar.jsx
+++ src/components/dashboard/SearchBar.jsx
@@ -6,20 +6,30 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
-    width: 400,
-    marginTop: 20
+    width: 800,
+    marginTop: 120,
+    borderRadius: 15
+  },
+  formControl: {
+    // margin: theme.spacing(1),
+    // minWidth: 120,
   },
   input: {
     marginLeft: theme.spacing(1),
     flex: 1,
   },
   iconButton: {
+    width: 100,
     padding: 10,
   },
   divider: {
@@ -30,22 +40,52 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SearchBar() {
   const classes = useStyles();
+  const [cuisine, setCuisine] = React.useState('All');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event) => {
+    setCuisine(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
   return (
     <div style={{ backgroundColor: 'gray', height: "100vh", display: 'flex', justifyContent: 'center', alignItems: 'flex-start' }}>
       <Paper component="form" className={classes.root}>
+        <FormControl className={classes.iconButton}>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={open}
+            onClose={handleClose}
+            onOpen={handleOpen}
+            value={cuisine}
+            onChange={handleChange}
+          >
+            <MenuItem value="All">
+              <em>All</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Divider className={classes.divider} orientation="vertical" />
 
         <InputBase
           className={classes.input}
-          placeholder="Search Google Maps"
-          inputProps={{ 'aria-label': 'search google maps' }}
+          placeholder="Search for a business"
+          inputProps={{ 'aria-label': 'search for a business' }}
         />
-        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+        <IconButton type="submit" className={{ ...classes.iconButton, paddingRight: 0 }} aria-label="search">
           <SearchIcon />
-        </IconButton>
-        <Divider className={classes.divider} orientation="vertical" />
-        <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-          <DirectionsIcon />
         </IconButton>
       </Paper>
     </div>
